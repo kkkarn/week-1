7b2239fb6017df7246172981e50ef20359a494a2
ze.mandkarn@gmail.com <ze.mandkarn@gmail.com>
2015-08-21 15:57:38 +0700

    done everythings

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,12 +10,29 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
+            input();
+
+            Console.ReadKey();
+        }
+
+        static public void input()
+        {
+            //int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            Console.WriteLine("Enter size :");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            int[] input = new int[x];
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                Console.WriteLine("Enter number :");
+                input[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            process(input);
+        }
+        static public void process(int[] input)
+        {
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
@@ -30,14 +47,18 @@ namespace Lab1_2
                     }
                 }
             }
-
+            output(input);
+        }
+        static public void output(int[] input)
+        {
             // output
             for (int i = 0; i != input.Length; i++)
             {
                 Console.Write(input[i]);
                 Console.Write(" ");
             }
-            Console.ReadKey();
-        }
+         }
+    
+
     }
 }
